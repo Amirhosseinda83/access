c534003569e8864a822491ea0056398b78b04eb3
Amirhossein <amirhosseinda83@gmail.com>
2025-03-08 08:28:59 +0330

    first

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'shabnam':'shabnam',
+        'tello':'tello'
+      }
+    },
+  },
+  plugins: [],
+}
+
