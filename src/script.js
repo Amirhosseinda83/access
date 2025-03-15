11d6e22dc86a0c28c9d28116b8a008a4ec1904d9
Amirhossein <amirhosseinda83@gmail.com>
2025-03-15 09:35:31 +0330

    first commit

---
+++ src/script.js
@@ -0,0 +1,41 @@
+// let firstCode = "firstAmir";
+// let first = 799000
+// let solve= 500000
+// let toman = "تومان"
+
+
+
+
+
+
+
+
+// document.querySelector('.send').addEventListener('click', function() {
+//     let  last = first - solve
+//     let discount = document.querySelector('.discount').value;
+// if( discount===firstCode  )   {
+//     document.querySelector('.copon').textContent="😊 دارید";
+//     document.querySelector('.solve').textContent= "500,000 تومان";
+//     document.querySelector('.last').textContent= first - solve + toman
+// } else{
+//     document.querySelector('.copon').textContent="متاسفم اشتباه بود !🥺"
+// }
+
+// }  )
+
+
+
+// document.querySelector('.modal').addEventListener('click', function() {
+
+// document.querySelector('.modal-container').classList.remove('hidden')
+
+
+// })
+
+// document.querySelector('.close-btn').addEventListener('click' , function() {
+
+// document.querySelector('.modal-container').classList.add('hidden')
+
+// }  )
+
+
